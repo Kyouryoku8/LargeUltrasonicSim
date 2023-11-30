0e64a115162c1c20a7a5d5711d3af423349c1240
Hayden Thurman <149443288+HThurman23@users.noreply.github.com>
2023-11-30 03:28:35 -0600

    Create output_files_tree_view.py

---
+++ main/output_files_tree_view.py
@@ -0,0 +1,36 @@
+from PyQt5.QtWidgets import QTreeView, QMenu
+from file_utils import FileUtils
+
+class OutputFilesTreeView(QTreeView):
+    def contextMenuEvent(self, event):
+        menu = QMenu(self)
+        viewAction = menu.addAction("View")
+        visualizeAction = menu.addAction("Visualize")
+        deleteAction = menu.addAction("Delete")
+        action = menu.exec_(self.mapToGlobal(event.pos()))
+
+        # Connect actions to your functions
+        if action == viewAction:
+            self.viewFile()
+        elif action == visualizeAction:
+            self.visualizeFile()
+        elif action == deleteAction:
+            self.deleteFile()
+
+    def viewFile(self):
+        # Implement view functionality
+        pass
+
+    def visualizeFile(self):
+        # Implement visualize functionality
+        pass
+
+    def deleteFile(self):
+        index = self.currentIndex()
+        if index.isValid():
+            model = self.model()
+            fileInfo = model.fileInfo(index)
+            if fileInfo.isFile():
+                filePath = fileInfo.absoluteFilePath()
+                success, message = FileUtils.deleteFile(filePath)
+                # Handle the response, e.g., show a message box or update status
